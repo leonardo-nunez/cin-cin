2e43c8a9dd51e35685ac05010082941ec63bf2b7
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2021-11-11 16:11:20 +0100

    card has ingredients now

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,59 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-const CocktailCard = props => {
+const CocktailCard = ({ cocktailsData }) => {
+
+  const cocktailName = cocktailsData.drinks[0].strDrink;
+  const image = cocktailsData.drinks[0].strDrinkThumb;
+  const instructions = cocktailsData.drinks[0].strInstructions
+
   return (
     <>
-      <h1>{props.cocktailsData.drinks[0].strDrink}</h1>
-      <img src={props.cocktailsData.drinks[0].strDrinkThumb} alt="" height="200px" width="200px" />
-      <p>{props.cocktailsData.drinks[0].strInstructions}</p>
+      <h1>{cocktailName}</h1>
+      <img src={image} alt="" height="200px" width="200px" />
+      <h3>Ingredients</h3>
+      <Ingredients cocktailsData={cocktailsData} />
+
+      <p>{instructions}</p>
+    </>
+  )
+}
+
+const Ingredients = ({ cocktailsData }) => {
+  const cocktailObject = cocktailsData.drinks[0]
+
+  const fetchedIngedients = 
+    Object.keys(cocktailObject)
+      .filter((key) => key.includes('strIngredient'))
+      .reduce((obj, key) => {
+        obj[key] = cocktailObject[key];
+        return obj;
+      }, {})
+  const ingredients = Object.keys(fetchedIngedients)
+  .filter((k) => fetchedIngedients[k] != null)
+  .reduce((a, k) => ({ ...a, [k]: fetchedIngedients[k] }), {});
+  const ingredientsArr = Object.values(ingredients)
+
+  const fetchedMeasures = 
+    Object.keys(cocktailObject)
+      .filter((key) => key.includes('strMeasure'))
+      .reduce((obj, key) => {
+        obj[key] = cocktailObject[key];
+        return obj;
+      }, {})
+  const measures = Object.keys(fetchedMeasures)
+  .filter((k) => fetchedMeasures[k] != null)
+  .reduce((a, k) => ({ ...a, [k]: fetchedMeasures[k] }), {});
+  const measuresArr = Object.values(measures)
+      
+  return(
+    <>
+      <ul>
+       {ingredientsArr.map((ingredient, index) => <li key={index}>{ingredient}</li>)}
+      </ul>
+      <ul>
+       {measuresArr.map((measure, index) => <li key={index}>{measure}</li>)}
+      </ul>
     </>
   )
 }
@@ -19,17 +66,21 @@ const App = () => {
   //   return cocktailsData ? console.log(cocktailsData.drinks[0].strDrinkThumb) : null
   // },[cocktailsData])
 
-  const getCocktail = async () => {
+  const getCocktail = async (e) => {
+    e.preventDefault();
     const response = await fetch('http://localhost:5000/api/cocktail/' + query)
     const body = await response.json();
     setCocktailsData(body)
+    setQuery('')
   };
 
   return (
     <div className="app">
       <h1>Cocktail Search</h1>
-      <input value={query} onChange={e => setQuery(e.target.value)} />
-      <button onClick={getCocktail}>Find Cocktail</button><br/>
+      <form onSubmit={getCocktail}>
+        <input value={query} onChange={e => setQuery(e.target.value)} />
+        <button>Find Cocktail</button><br/>
+      </form>
 
       {cocktailsData ? <CocktailCard cocktailsData={cocktailsData} /> : null}
     </div>
