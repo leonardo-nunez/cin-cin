0439ec0e949cc492ebb645952df6f46bd12a23ef
Leo <lnpb@hotmail.com>
2022-06-27 17:29:08 +0200

    trying something in server.js

--- server.js
+++ server.js
@@ -16,6 +16,17 @@ app.get('/api/cocktail/:query', (req, res) => {
     },
   };
 
+  if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
   res.header('Access-Control-Allow-Origin', '*');
 
   axios(config)
